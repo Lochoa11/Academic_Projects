95f581fa3382e91044174944bd1cc0b42b883b76
Lin <linochoa11690@gmail.com>
2017-08-21 21:58:00 -0400

    added physics 204 project and computer graphics projects

---
+++ CS368_Computer_Graphics/Project3/Project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Lin Ochoa on 8/21/17.
+//  Copyright © 2017 Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
